52adf1f3fb6a0699dab350e443eb53694dff026a
Fibiolla Plaath <Fibiolla.Plaath@green.com.na>
2025-05-02 16:48:20 +0200

    service files added

--- swakopApp/service/src/main/java/com/example/swakopmundapp/service/ServiceInit.kt
+++ swakopApp/service/src/main/java/com/example/swakopmundapp/service/ServiceInit.kt
@@ -1,2 +1,14 @@
 package com.example.swakopmundapp.service
 
+import com.example.swakopmundapp.service.client.BasicAuthInterceptor
+import com.example.swakopmundapp.service.client.OkHttpClient
+import com.example.swakopmundapp.service.client.RetrofitClient
+import com.example.swakopmundapp.service.client.UnsafeOkHttpClient
+import org.koin.dsl.module
+
+val serviceModule = module {
+    single { UnsafeOkHttpClient() }
+    single { OkHttpClient(get(), get()) }
+    single { BasicAuthInterceptor() }
+    single { RetrofitClient(get()).instance }
+}
---
+++ swakopApp/service/src/main/java/com/example/swakopmundapp/service/remoteRepoInt/RemoteAuthenticationRepoInt.kt
@@ -0,0 +1,9 @@
+package com.example.swakopmundapp.service.remoteRepoInt
+
+interface RemoteAuthenticationRepoInt {
+
+    /**
+     * LOGIN
+     */
+
+}

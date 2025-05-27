9a919b917bcd91a1fe9c1663e7177df22626077c
Taloshili Nghiwewelekwa <140718600+taloshiliN@users.noreply.github.com>
2025-05-27 13:47:53 +0200

    Update TourismGridScreen.kt
    Update Tourism Screen

--- swakopApp/app/src/main/java/com/example/swakopmundapp/ui/tourism/TourismGridScreen.kt
+++ swakopApp/app/src/main/java/com/example/swakopmundapp/ui/tourism/TourismGridScreen.kt
@@ -1,46 +1,105 @@
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.swakopmundapp.R
 import com.example.swakopmundapp.data.model.tourism.TourismViewModel
+import com.example.swakopmundapp.ui.about.AboutScreen
 
 @Composable
-fun TourismGridScreen(navController: NavController, viewModel: TourismViewModel) {
-    LazyVerticalGrid(columns = GridCells.Fixed(2), contentPadding = PaddingValues(8.dp)) {
-        items(viewModel.activities) { activity ->
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
+fun TourismGridScreen(
+    navController: NavController,
+    viewModel: TourismViewModel,
+    onBack: () -> Unit = {}
+) {
+    Box(modifier = Modifier.fillMaxSize()){
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
                 modifier = Modifier
-                    .padding(8.dp)
-                    .clickable {
-                        navController.navigate("tourism_detail/${activity.name}")
-                    }
+                    .fillMaxWidth()
+                    .height(65.dp)
+                    .background(colorResource(id = R.color.bluebar)),
+                contentAlignment = Alignment.Center
             ) {
-                Image(
-                    painter = painterResource(id = activity.imageResId),
-                    contentDescription = activity.name,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(150.dp)
+                IconButton(
+                    onClick = onBack,
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 16.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        tint = Color.White
+                    )
+                }
+                Text(
+                    text = "Tourism",
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.align(Alignment.Center)
                 )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = activity.name)
+            }
+            LazyVerticalGrid(columns = GridCells.Fixed(2), contentPadding = PaddingValues(8.dp)) {
+                items(viewModel.activities) { activity ->
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .clickable {
+                                navController.navigate("tourism_detail/${activity.name}")
+                            }
+                    ) {
+                        Image(
+                            painter = painterResource(id = activity.imageResId),
+                            contentDescription = activity.name,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.size(150.dp)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(text = activity.name)
+                    }
+                }
             }
         }
     }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun TourismGridScreenPreview() {
+//    TourismGridScreen(
+//        navController = TODO(),
+//        viewModel = TODO(),
+//    )
+//}

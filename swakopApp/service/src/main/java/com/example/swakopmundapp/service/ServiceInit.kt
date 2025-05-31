b2fcf5b8d8ec91dce260cc8e10c64b082ade5535
Fibiolla Plaath <Fibiolla.Plaath@green.com.na>
2025-05-31 12:52:56 +0200

    signup and login logic flow done

--- swakopApp/service/src/main/java/com/example/swakopmundapp/service/ServiceInit.kt
+++ swakopApp/service/src/main/java/com/example/swakopmundapp/service/ServiceInit.kt
@@ -4,6 +4,8 @@ import com.example.swakopmundapp.service.client.BasicAuthInterceptor
 import com.example.swakopmundapp.service.client.OkHttpClient
 import com.example.swakopmundapp.service.client.RetrofitClient
 import com.example.swakopmundapp.service.client.UnsafeOkHttpClient
+import com.example.swakopmundapp.service.remoteRepoImp.RemoteAuthenticationRepoImp
+import com.example.swakopmundapp.service.remoteRepoInt.RemoteAuthenticationRepoInt
 import org.koin.dsl.module
 
 val serviceModule = module {
@@ -11,4 +13,6 @@ val serviceModule = module {
     single { OkHttpClient(get(), get()) }
     single { BasicAuthInterceptor() }
     single { RetrofitClient(get()).instance }
+
+    single <RemoteAuthenticationRepoInt> { RemoteAuthenticationRepoImp(get()) }
 }

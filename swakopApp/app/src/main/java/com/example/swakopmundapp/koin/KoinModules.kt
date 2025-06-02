eb92bdfe5bb9bba4a746a7a661538c0c6d22ae42
Chilwalo Owen Luyando <chilwaloowen130@gmail.com>
2025-06-02 11:13:40 +0200

    fixed the weather crash
    fixed the crash that was happening when navigating to the weather screen

--- swakopApp/app/src/main/java/com/example/swakopmundapp/koin/KoinModules.kt
+++ swakopApp/app/src/main/java/com/example/swakopmundapp/koin/KoinModules.kt
@@ -5,9 +5,10 @@ import com.example.swakopmundapp.data.repository.TourismRepository
 import com.example.swakopmundapp.repository.AuthenticationRepository
 import com.example.swakopmundapp.repository.UserRepository
 import com.example.swakopmundapp.ui.currency.ExchangeViewModel
+import com.example.swakopmundapp.ui.municipal.ReportIssueViewModel
+import com.example.swakopmundapp.ui.weather.WeatherViewModel
 import com.example.swakopmundapp.viewModel.AuthenticationViewModel
 import com.example.swakopmundapp.viewModel.UserViewModel
-import com.example.swakopmundapp.ui.municipal.ReportIssueViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -23,6 +24,7 @@ val viewModelModule = module {
     viewModel { ExchangeViewModel(get()) }
     viewModel { TourismViewModel(get()) }
     viewModel { ReportIssueViewModel(get()) }
+    viewModel { WeatherViewModel(get())}
 
 }
 
